2c06c70b92bb55e1b6b384b8927eea6ddde74030
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-11 23:15:35 +0530

    Time: 19 ms (99.2%), Space: 9.2 MB (98.16%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,30 +1,61 @@
 
 //======================== Tabulation Approach ===========================
 
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+//         int n = s.size();
+//         vector<vector<int>> dp(n, vector<int>(n, 0));
+//         for(int i = 0; i < n; i++)
+//         {
+//             dp[i][i] = 1; //single character
+//         }
+//         for(int len = 2; len <= n; len++)
+//         {
+//             for(int i = 0; i <= n - len; i++)
+//             {
+//                 int j = i + len - 1;
+//                 if(s[i] == s[j])
+//                 {
+//                     dp[i][j] = 2 + dp[i + 1][j - 1];
+//                 }
+//                 else
+//                 {
+//                     dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+//                 }
+//             }
+//         }
+//         return dp[0][n - 1];
+//     }
+// };
+
+
+//========================== Space Optimization ========================
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> prev(n, 0), curr(n, 0);
         for(int i = 0; i < n; i++)
         {
-            dp[i][i] = 1; //single character
+            prev[i] = 1; //single character
         }
-        for(int len = 2; len <= n; len++)
+        for(int i = n - 1; i >= 0; i--)
         {
-            for(int i = 0; i <= n - len; i++)
+            curr[i] = 1;
+            for(int j = i + 1; j < n; j++)
             {
-                int j = i + len - 1;
                 if(s[i] == s[j])
                 {
-                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                    curr[j] = 2 + prev[j - 1];
                 }
                 else
                 {
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                    curr[j] = max(prev[j], curr[j - 1]);
                 }
             }
+            prev = curr;
         }
-        return dp[0][n - 1];
+        return prev[n - 1];
     }
 };

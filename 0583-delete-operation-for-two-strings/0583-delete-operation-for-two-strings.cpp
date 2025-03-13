e04ea785afc96d254a23f796973d5a34da3fb31a
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-13 19:53:42 +0530

    Time: 9 ms (73.56%), Space: 11.7 MB (84.6%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,33 +1,71 @@
+
+//========================= Tabulation Approach ======================
+
+// class Solution {
+// public:
+//     int helper(string word1, string word2)
+//     {
+//         int m = word1.size();
+//         int n = word2.size();
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+//         for(int i = 0; i <= m; i++)
+//         {
+//             dp[i][0] = 0;
+//         }
+//         for(int j = 0; j <= n; j++)
+//         {
+//             dp[0][j] = 0;
+//         }
+//         for(int i = 1; i <= m; i++)
+//         {
+//             for(int j = 1; j <= n; j++)
+//             {
+//                 if(word1[i - 1] == word2[j - 1])
+//                 {
+//                     dp[i][j] = 1 + dp[i - 1][j - 1];
+//                 }
+//                 else
+//                 {
+//                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+//     int minDistance(string word1, string word2) {
+//         int m = word1.size();
+//         int n = word2.size();
+//        int k = helper(word1, word2);
+//        return (m - k) + (n - k);
+//     }
+// };
+
+
+//========================= Space Optimization ======================
 class Solution {
 public:
     int helper(string word1, string word2)
     {
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        for(int i = 0; i <= m; i++)
-        {
-            dp[i][0] = 0;
-        }
-        for(int j = 0; j <= n; j++)
-        {
-            dp[0][j] = 0;
-        }
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+
         for(int i = 1; i <= m; i++)
         {
             for(int j = 1; j <= n; j++)
             {
                 if(word1[i - 1] == word2[j - 1])
                 {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 }
                 else
                 {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = max(prev[j], curr[j - 1]);
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
     int minDistance(string word1, string word2) {
         int m = word1.size();

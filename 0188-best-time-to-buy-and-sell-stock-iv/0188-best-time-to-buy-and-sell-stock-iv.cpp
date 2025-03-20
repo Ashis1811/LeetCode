58b2d6b9e9c9bccb662b20e9c1eca970b868862b
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-20 21:08:07 +0530

    Time: 15 ms (31.77%), Space: 18.4 MB (22.76%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,5 +1,27 @@
-
-
+class Solution {
+public:
+ 
+    int helper(int ind, int buy, int cap, vector<int>& prices, int n, vector<vector<vector<int>>>& dp)
+    {
+        if(ind == n || cap == 0) return 0;
+        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+        int profit = 0;
+        if(buy)
+        {
+            profit = max(- prices[ind] + helper(ind + 1, 0, cap, prices, n, dp), 0 + helper(ind + 1, 1, cap, prices, n, dp));
+        }
+        else
+        {
+            profit = max(prices[ind] + helper(ind + 1, 1, cap - 1, prices, n, dp), 0 + helper(ind + 1, 0, cap, prices, n, dp));
+        }
+        return dp[ind][buy][cap] = profit;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        return helper(0, 1, k, prices, n, dp);
+    }
+};
 
 
 //================================= Tabulation Approach ===============================
@@ -33,33 +55,33 @@
 
 
 //=============================== Space Optimization ===============================
-class Solution {
-public:
-    int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> prev(2, vector<int>(k + 1, 0)), curr(2, vector<int>(k + 1, 0));
-        for(int ind = n - 1; ind >= 0; ind--)
-        {
-            for(int buy = 0; buy <= 1; buy++)
-            {
-                for(int cap = 1; cap <= k; cap++)
-                {
-                    int profit = 0;
-                    if(buy == 1)
-                    {
-                        curr[buy][cap] = max(- prices[ind] + prev[0][cap], 0 + prev[1][cap]);
-                    }
-                    else
-                    {
-                        curr[buy][cap] = max(prices[ind] + prev[1][cap - 1], 0 + prev[0][cap]);
-                    }
+// class Solution {
+// public:
+//     int maxProfit(int k, vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> prev(2, vector<int>(k + 1, 0)), curr(2, vector<int>(k + 1, 0));
+//         for(int ind = n - 1; ind >= 0; ind--)
+//         {
+//             for(int buy = 0; buy <= 1; buy++)
+//             {
+//                 for(int cap = 1; cap <= k; cap++)
+//                 {
+//                     int profit = 0;
+//                     if(buy == 1)
+//                     {
+//                         curr[buy][cap] = max(- prices[ind] + prev[0][cap], 0 + prev[1][cap]);
+//                     }
+//                     else
+//                     {
+//                         curr[buy][cap] = max(prices[ind] + prev[1][cap - 1], 0 + prev[0][cap]);
+//                     }
                     
-                }
+//                 }
                 
-            }
-            prev = curr;
+//             }
+//             prev = curr;
             
-        }
-        return prev[1][k];
-    }
-};
+//         }
+//         return prev[1][k];
+//     }
+// };

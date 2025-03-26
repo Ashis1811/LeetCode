3ba6fb15e493c5f028a2b329099bc88cf2bec5ff
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-26 23:42:12 +0530

    Time: 52 ms (27.96%), Space: 99.8 MB (6.38%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,33 +1,66 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<int> list;
-        // 2-D array to 1-D array
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> ans;
         for(auto row : grid)
         {
             for(auto col : row)
             {
-                list.push_back(col);
+                ans.push_back(col);
             }
         }
-        // Check if possible to divide by x
-        int remainder = list[0] % x;
-        for(auto num : list)
+        int remainder = ans[0] % x;
+        for(auto num : ans)
         {
             if(num % x != remainder)
             {
                 return -1;
             }
         }
-        sort(list.begin(), list.end());
-        int mid = list[list.size() / 2];
+        sort(ans.begin(), ans.end());
+        int mid = ans[ans.size() / 2];
         int op = 0;
-        for(auto num : list)
+        for(auto it : ans)
         {
-            op += abs(num - mid) / x;
+            op += abs(it - mid) / x;
         }
         return op;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minOperations(vector<vector<int>>& grid, int x) {
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         vector<int> list;
+//         // 2-D array to 1-D array
+//         for(auto row : grid)
+//         {
+//             for(auto col : row)
+//             {
+//                 list.push_back(col);
+//             }
+//         }
+//         // Check if possible to divide by x
+//         int remainder = list[0] % x;
+//         for(auto num : list)
+//         {
+//             if(num % x != remainder)
+//             {
+//                 return -1;
+//             }
+//         }
+//         sort(list.begin(), list.end());
+//         int mid = list[list.size() / 2];
+//         int op = 0;
+//         for(auto num : list)
+//         {
+//             op += abs(num - mid) / x;
+//         }
+//         return op;
+//     }
+// };

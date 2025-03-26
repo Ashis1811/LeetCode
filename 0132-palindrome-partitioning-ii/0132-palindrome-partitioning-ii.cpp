33f6731ea5f383ac34c7b08fff7b8aec77b295f1
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-26 20:28:21 +0530

    Time: 1925 ms (5.02%), Space: 9.4 MB (77.23%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,6 +1,42 @@
 
 //================================== Memoization Approach ===========================
 
+// class Solution {
+// public:
+//     bool isPalindrome(int start, int end, string& s)
+//     {
+//         while(start <= end)
+//         {
+//             if(s[start++] != s[end--])
+            
+//                 return false;              
+//         }
+//         return true;
+//     }
+    
+//     int f(int i, string& s, vector<int>& dp)
+//     {
+//         if(i == s.size()) return 0;
+//         if(dp[i] != -1) return dp[i];
+//         int minCost = INT_MAX;
+//         for(int j = i; j < s.size(); j++)
+//         {
+//             if(isPalindrome(i, j, s))
+//             {
+//                 int cost = 1 + f(j + 1, s, dp);
+//                 minCost = min(minCost, cost);
+//             }
+//         }
+//         return dp[i] = minCost;
+//     }
+
+//     int minCut(string s) {
+//         int n = s.size();
+//         vector<int> dp(n, -1);
+//         return f(0, s, dp) - 1;
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(int start, int end, string& s)
@@ -13,28 +49,25 @@ public:
         }
         return true;
     }
-    
-    int f(int i, string& s, vector<int>& dp)
-    {
-        if(i == s.size()) return 0;
-        if(dp[i] != -1) return dp[i];
-        int minCost = INT_MAX;
-        for(int j = i; j < s.size(); j++)
-        {
-            if(isPalindrome(i, j, s))
-            {
-                int cost = 1 + f(j + 1, s, dp);
-                minCost = min(minCost, cost);
-            }
-        }
-        return dp[i] = minCost;
-    }
 
     int minCut(string s) {
         int n = s.size();
-        if(n == 1) return 0;
-        vector<int> dp(n, -1);
-        return f(0, s, dp) - 1;
+        vector<int> dp(n, 0);
+
+        for(int ind = n - 1; ind >= 0; ind--)
+        {
+             int minCost = INT_MAX;
+            for(int j = ind; j < n; j++)
+            {
+                if(isPalindrome(ind, j, s))
+                {
+                    int cost = 1 +(j + 1 < n ? dp[j + 1] : 0);
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[ind] = minCost;
+        }
+        return dp[0] - 1;
     }
 };
 

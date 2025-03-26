62949302954b4a43c17732342002e3c1d3267ca3
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-27 00:49:41 +0530

    Time: 4 ms (72.9%), Space: 12.4 MB (59.26%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,55 @@
+
+//============================ Memoization Approach =================================
+
+// class Solution {
+// public:
+//     int f(int ind, vector<int>& arr, int k, vector<int>& dp)
+//     {
+//         if(ind == arr.size()) return 0;
+//         if(dp[ind] != -1) return dp[ind];
+//         int len = 0;
+//         int maxi = INT_MIN;
+//         int maxAns = INT_MIN;
+//         for(int j = ind; j < min((int)arr.size(), ind + k); j++)
+//         {
+//             len++;
+//             maxi = max(maxi, arr[j]);
+//             int sum = len * maxi + f(j + 1, arr, k, dp);
+//             maxAns = max(maxAns, sum);
+//         }
+//         return dp[ind] = maxAns;
+//     }
+//     int maxSumAfterPartitioning(vector<int>& arr, int k) {
+//         int n = arr.size();
+//         vector<int> dp(n, -1);
+//         return f(0, arr, k, dp);
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    int f(int ind, vector<int>& arr, int k, vector<int>& dp)
-    {
-        if(ind == arr.size()) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int len = 0;
-        int maxi = INT_MIN;
-        int maxAns = INT_MIN;
-        for(int j = ind; j < min((int)arr.size(), ind + k); j++)
-        {
-            len++;
-            maxi = max(maxi, arr[j]);
-            int sum = len * maxi + f(j + 1, arr, k, dp);
-            maxAns = max(maxAns, sum);
-        }
-        return dp[ind] = maxAns;
-    }
+   
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n, -1);
-        return f(0, arr, k, dp);
+        vector<int> dp(n + 1, 0);
+        
+        for(int ind = n - 1; ind >= 0; ind--)
+        {
+            int len = 0;
+            int maxi = INT_MIN;
+            int maxAns = INT_MIN;
+            for(int j = ind; j < min(n, ind + k); j++)
+            {
+                len++;
+                maxi = max(maxi, arr[j]);
+                int sum = len * maxi + dp[j + 1];
+                maxAns = max(maxAns, sum);
+            }
+            dp[ind] = maxAns;
+        }        
+        return dp[0];
     }
 };

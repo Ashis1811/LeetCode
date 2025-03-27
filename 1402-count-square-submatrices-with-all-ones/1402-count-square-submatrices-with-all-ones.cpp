175caa274514858b4e0797d5a5654fef773967c2
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-27 14:26:28 +0530

    Time: 7 ms (53.47%), Space: 30.3 MB (43.09%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,28 +1,60 @@
 
 //======================= Memoization Approach ==========================
 
+// class Solution {
+// public:
+//     int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp)
+//     {
+//         if(i < 0 || j < 0) return 0;
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(matrix[i][j] == 0) return dp[i][j] = 0;
+//         int top = f(i - 1, j, matrix, dp);
+//         int left = f(i, j - 1, matrix, dp);
+//         int diagonal = f(i - 1, j - 1, matrix, dp);
+//         return dp[i][j] = min({top, left, diagonal}) + 1;
+//     }
+//     int countSquares(vector<vector<int>>& matrix) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+//         int square = 0;
+//         for(int i = 0; i < m; i++)
+//         {
+//             for(int j = 0; j < n; j++)
+//             {
+//                 square += f(i, j, matrix, dp);
+//             }
+//         }
+//         return square;
+//     }
+// };
+
+
+//============================== Tabulation Approach ==========================
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp)
-    {
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(matrix[i][j] == 0) return dp[i][j] = 0;
-        int top = f(i - 1, j, matrix, dp);
-        int left = f(i, j - 1, matrix, dp);
-        int diagonal = f(i - 1, j - 1, matrix, dp);
-        return dp[i][j] = min({top, left, diagonal}) + 1;
-    }
+
     int countSquares(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
         int square = 0;
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                square += f(i, j, matrix, dp);
+                if(matrix[i][j] == 1)
+                {
+                    if(i == 0 || j == 0)
+                    {
+                        dp[i][j] = 1;
+                    }
+                    else
+                    {
+                        dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                    }                    
+                } 
+                square += dp[i][j];   
             }
         }
         return square;

3f3e3817dc19d17703bd522fff23ce149c1d84f2
Ashis Shaw <ashisshaw1811@gmail.com>
2025-03-31 12:24:47 +0530

    Time: 41 ms (32.49%), Space: 67.3 MB (67.09%) - LeetHub

---
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        vector<int> ans;
+        for(int i = 0; i < n - 1; i++)
+        {
+            int sum = weights[i] + weights[i + 1];
+            ans.push_back(sum);
+        }
+        sort(ans.begin(), ans.end());
+        long long min_sum = 0, max_sum = 0;
+        for(int i = 0; i < k - 1; i++)
+        {
+            min_sum += ans[i];
+            max_sum += ans[n - 2 - i];
+        }
+        return max_sum - min_sum;
+    }
+};

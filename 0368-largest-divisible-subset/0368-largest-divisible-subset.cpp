2c6f9bbc60a8582fe4d9e2995178bf6ae9466f02
Ashis Shaw <ashisshaw1811@gmail.com>
2025-04-06 12:05:30 +0530

    Time: 12 ms (45.23%), Space: 12.7 MB (47.06%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
+        int maxLen = 1, lastInd = 0;
         vector<int> dp(n, 1);
         vector<int> prev(n, -1);
-        int maxLen = 1, lastInd = 0;
         for(int i = 1; i < n; i++)
         {
             for(int j = 0; j < i; j++)
@@ -29,6 +29,5 @@ public:
         }
         reverse(ans.begin(), ans.end());
         return ans;
-
     }
 };

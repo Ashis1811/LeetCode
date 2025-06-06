175658698c9211648a6bd89054c03132179d8760
Ashis Shaw <ashisshaw1811@gmail.com>
2025-06-06 16:39:23 +0530

    Time: 47 ms (24.47%), Space: 32.2 MB (57.23%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if(n % groupSize != 0) return false;
+        map<int, int> freq;
+        for(auto it : hand)
+        {
+            freq[it]++;
+        }
+        for(auto [num, count] : freq)
+        {
+            if(count > 0)
+            {
+                for(int i = 0; i < groupSize; i++)
+                {
+                    if(freq[num + i] < count)
+                        return false;
+                        freq[num + i] -= count;
+                }
+            }
+        }
+        return true;
+    }
+};

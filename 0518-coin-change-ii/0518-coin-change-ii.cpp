7688268fa92060a38576e8d22de837030002f217
Ashis Shaw <ashisshaw1811@gmail.com>
2025-06-17 13:29:29 +0530

    Time: 35 ms (18.82%), Space: 28.5 MB (45.49%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,17 @@
-
-//====================== Memorization Approach ========================
-
 class Solution {
 public:
-    int helper(int amount, int ind, vector<int>& coins, vector<vector<int>>& dp)
+    int helper(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp)
     {
         if(amount == 0) return 1;
         if(ind >= coins.size() || amount < 0) return 0;
         if(dp[ind][amount] != -1) return dp[ind][amount];
-
-        int include = helper(amount - coins[ind], ind, coins, dp);
-        int exclude = helper(amount, ind + 1, coins, dp);
-
-        return dp[ind][amount] = include + exclude;
+        int take = helper(ind, amount - coins[ind], coins, dp);
+        int nontake = helper(ind + 1, amount, coins, dp);
+        return dp[ind][amount] = take + nontake;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        return helper(amount, 0, coins, dp);
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return helper(0, amount, coins, dp);
     }
 };

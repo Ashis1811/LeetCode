b04a72285bb6cee1c120ce6880fe5d3873eded38
Ashis Shaw <ashisshaw1811@gmail.com>
2025-07-03 12:55:56 +0530

    Time: 94 ms (27.01%), Space: 76 MB (20.81%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,61 +1,32 @@
-
-//======================== Tabulation Approach ===========================
-
-// class Solution {
-// public:
-//     int longestPalindromeSubseq(string s) {
-//         int n = s.size();
-//         vector<vector<int>> dp(n, vector<int>(n, 0));
-//         for(int i = 0; i < n; i++)
-//         {
-//             dp[i][i] = 1; //single character
-//         }
-//         for(int len = 2; len <= n; len++)
-//         {
-//             for(int i = 0; i <= n - len; i++)
-//             {
-//                 int j = i + len - 1;
-//                 if(s[i] == s[j])
-//                 {
-//                     dp[i][j] = 2 + dp[i + 1][j - 1];
-//                 }
-//                 else
-//                 {
-//                     dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
-//                 }
-//             }
-//         }
-//         return dp[0][n - 1];
-//     }
-// };
-
-
-//========================== Space Optimization ========================
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        vector<int> prev(n, 0), curr(n, 0);
-        for(int i = 0; i < n; i++)
+    int longest(string s, string t)
+    {
+        int m = s.size();
+        int n = t.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        for(int i = 0; i < m; i++) dp[i][0] = 0;
+        for(int j = 0; j < n; j++) dp[0][j] = 0;
+
+        for(int i = 1; i <= m; i++)
         {
-            prev[i] = 1; //single character
-        }
-        for(int i = n - 1; i >= 0; i--)
-        {
-            curr[i] = 1;
-            for(int j = i + 1; j < n; j++)
+            for(int j = 1; j <= n; j++)
             {
-                if(s[i] == s[j])
+                if(s[i - 1] == t[j - 1])
                 {
-                    curr[j] = 2 + prev[j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else
                 {
-                    curr[j] = max(prev[j], curr[j - 1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
-            prev = curr;
         }
-        return prev[n - 1];
+        return dp[m][n];
+    }
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+        return longest(s, t);
     }
 };

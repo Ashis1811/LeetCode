c27be8a6ed627bf3cc7d0b49cb32645053ec13c4
Ashis Shaw <ashisshaw1811@gmail.com>
2025-08-19 15:16:21 +0530

    Time: 4 ms (27.82%), Space: 111.3 MB (74.5%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count = 0, res = 0;
+
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 0)
+            {
+                count++;
+                res += count;
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        return res;
+    }
+};
